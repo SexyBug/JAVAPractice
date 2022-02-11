ddc0e2e5176138b6e3a97f01f821fa791d62d5e0
SexyBug <2921978909@qq.com>
2022-02-11 15:31:25 +0800

    反射

--- src/my/tool/TestReflection.java
+++ src/my/tool/TestReflection.java
@@ -2,6 +2,7 @@ package my.tool;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 //反射实践，参考：https://www.sczyh30.com/posts/Java/java-reflection-1/
 public class TestReflection {
@@ -12,15 +13,30 @@ public class TestReflection {
         Class<?> c= Class.forName(className);
 
         //获取构造函数
-        Constructor constructor1=c.getConstructor();
+        Constructor<?> constructor1=c.getConstructor();
         //使用构造函数实例化对象
-        Object obj1=constructor1.newInstance();
-        System.out.println(obj1);
+        Object a1=constructor1.newInstance();
+        System.out.println(a1);
 
         //获取构造函数
-        Constructor constructor2=c.getConstructor(Integer.class);
+        Constructor<?> constructor2=c.getConstructor(Integer.class);
         //使用构造函数实例化对象
-        Object obj2=constructor2.newInstance(1);
-        System.out.println(obj2);
+        Object a2=constructor2.newInstance(2);
+        System.out.println(a2);
+
+        //获取某个类的所有公用（public）方法，包括其继承类的公用方法。
+        Method method1=c.getMethod("setV",Integer.class);
+        Method method2=c.getMethod("getV");
+
+        //指定对象，调用方法
+        method1.invoke(a1,1);
+        Object obj3=method2.invoke(a1);
+        System.out.println(obj3);
+
+        method1.invoke(a2,22);
+        Object obj4=method2.invoke(a2);
+        System.out.println(obj4);
+
+
     }
 }
